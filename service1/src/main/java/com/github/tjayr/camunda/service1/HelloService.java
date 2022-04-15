c9f582378f824fa33de7801defb48bd643a3c8d2
Tony Ayres <tayres@gmail.com>
2022-04-15 10:53:11 +0100

    inital

---
+++ service1/src/main/java/com/github/tjayr/camunda/service1/HelloService.java
@@ -0,0 +1,25 @@
+package com.github.tjayr.camunda.service1;
+
+import io.camunda.zeebe.client.api.response.ActivatedJob;
+import io.camunda.zeebe.client.api.worker.JobClient;
+import io.camunda.zeebe.spring.client.annotation.ZeebeWorker;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+
+    @ZeebeWorker(type = "hello")
+    public void handleJobFoo(final JobClient client, final ActivatedJob job) {
+
+        System.out.println("Hello vars "+job.getVariablesAsMap());
+
+        client.newCompleteCommand(job.getKey())
+                .send()
+                .exceptionally( throwable -> { throw new RuntimeException("Could not complete job " + job, throwable); });
+    }
+
+    public void start() {
+
+    }
+
+}
